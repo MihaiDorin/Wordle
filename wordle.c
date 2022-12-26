4711776ac3ba2152b7e2272d1bec02b11e6fbbb6
Pacuraru Mihai Dorin <mihaidorin653@gmail.com>
2022-12-26 13:15:35 +0200

    Adaugare meniu

--- wordle.c
+++ wordle.c
@@ -1,5 +1,111 @@
-#include <ncurses.h>
+#include <menu.h>
+#include <curses.h>
+#include <stdlib.h>
+#include <string.h>
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 4
 
-int main(){
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color){	
+    int length, x, y;
+	float temp;
+	if(win == NULL)
+		win = stdscr;
+	getyx(win, y, x);
+	if(startx != 0)
+		x = startx;
+	if(starty != 0)
+		y = starty;
+	if(width == 0)
+		width = 80;
+
+	length = strlen(string);
+	temp = (width - length)/ 2;
+	x = startx + (int)temp;
+	wattron(win, color);
+	mvwprintw(win, y, x, "%s", string);
+	wattroff(win, color);
+	refresh();
+}
+
+int main(){	
+    ITEM **my_items;
+	int c, x, y;				
+	MENU *my_menu;
+    WINDOW *my_menu_win;
+    int n_choices, i;
+	char *choices[] = {"START JOC", "IESIRE JOC"};
+	/* Initialize curses */
+	initscr();
+    getmaxyx(stdscr, y, x);
+	start_color();
+    cbreak();
+    noecho();
+	keypad(stdscr, TRUE);
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+
+	/* Create items */
+    n_choices = ARRAY_SIZE(choices);
+    my_items = (ITEM **)calloc(n_choices, sizeof(ITEM *));
+    for(i = 0; i < n_choices; i++){
+        my_items[i] = new_item(choices[i], NULL);
+    }
+	/* Crate menu */
+	my_menu = new_menu((ITEM **)my_items);
+
+	/* Create the window to be associated with the menu */
+    my_menu_win = newwin(20, 80, 16, 53);
+    keypad(my_menu_win, TRUE);
+     
+	/* Set main window and sub window */
+    set_menu_win(my_menu, my_menu_win);
+    set_menu_sub(my_menu, derwin(my_menu_win, 2, 20, 9, 30));
+
+    wresize(stdscr, y, x);
+
+	/* Set menu mark to the string " * " */
+    set_menu_mark(my_menu, " > ");
+
+    /* Print a border around the main window and print a title */
+    box(my_menu_win, 0, 0);
+	print_in_middle(my_menu_win, 1, 4, 70, "BUN VENIT LA WORDLE! APASA START PENTRU A INCEPE JOCUL", COLOR_PAIR(1));
+	mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
+	mvwhline(my_menu_win, 2, 1, ACS_HLINE, 80);
+	mvwaddch(my_menu_win, 2, 79, ACS_RTEE);
+	refresh();
+        
+	/* Post the menu */
+	post_menu(my_menu);
+	wrefresh(my_menu_win);
+
+    curs_set(0);
+
+	while((c = wgetch(my_menu_win)) != 27)
+	{       
+        switch(c)
+	        {	case KEY_DOWN:
+				menu_driver(my_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(my_menu, REQ_UP_ITEM);
+				break;
+            case 10:
+                {	ITEM *cur;
+                    void (*p)(char *);
+                    cur = current_item(my_menu);
+                    p = item_userptr(cur);
+                    p((char *)item_name(cur));
+                    pos_menu_cursor(my_menu);
+                    break;
+                }
+            }
+        wrefresh(my_menu_win);
+	}	
+
+	/* Unpost and free all the memory taken up */
+    unpost_menu(my_menu);
+    free_menu(my_menu);
+    for(i = 0; i < n_choices; i++){
+        free_item(my_items[i]);}
+	endwin();
     return 0;
 }
