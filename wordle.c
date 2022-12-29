5d4d1f38d8639cd2ccf884e55ca334cd0b56fb33
Pacuraru Mihai Dorin <mihaidorin653@gmail.com>
2022-12-29 16:50:25 +0200

    Implementare colorare keyboard + afisare cuvinte

--- wordle.c
+++ wordle.c
@@ -2,8 +2,21 @@
 #include <curses.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
-#define CTRLD 4
+#define X 34
+#define Y 11
+#define Y1 31
+#define Y2 32
+#define Y3 33
+#define X1 30
+#define X2 31
+#define X3 33
+
+typedef struct{
+	char name;
+	int poz;
+}TASTA;
 
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color){	
     int length, x, y;
@@ -27,14 +40,65 @@ void print_in_middle(WINDOW *win, int starty, int startx, int width, char *strin
 	refresh();
 }
 
+int random_poz(int lower, int upper){
+    int num;
+	num = (rand() % (upper - lower + 1)) + lower;
+	return num;
+}
+
+int aparitie (char *word, char c){
+	int i;
+	for (i = 0; i < strlen(word); i++)
+		if (word[i] == c)
+			return 1;
+	return 0;
+}
+
+void color_keyboard(WINDOW *win, char *k1, char *k2, char *k3, char key){
+	int i;
+	for (i = 0; i < strlen(k1); i++)
+		if (tolower(k1[i]) == tolower(key)){
+			wattron(win, COLOR_PAIR(1));
+			if (key >= 'a' && key <= 'z')
+				mvwprintw(win, Y1, X1 + i, "%c", key - 32);
+			if (key >= 'A' && key <= 'Z')
+				mvwprintw(win, Y1, X1 + i, "%c", key);
+			wattroff(win, COLOR_PAIR(1));
+			wrefresh(win);
+		}
+	for (i = 0; i < strlen(k2); i++)
+		if (tolower(k2[i]) == tolower(key)){
+			wattron(win, COLOR_PAIR(1));
+			if (key >= 'a' && key <= 'z')
+				mvwprintw(win, Y2, X2 + i, "%c", key - 32);
+			if (key >= 'A' && key <= 'Z')
+				mvwprintw(win, Y2, X2 + i, "%c", key);
+			wattroff(win, COLOR_PAIR(1));
+			wrefresh(win);
+		}
+	for (i = 0; i < strlen(k3); i++)
+		if (tolower(k3[i]) == tolower(key)){
+			wattron(win, COLOR_PAIR(1));
+			if (key >= 'a' && key <= 'z')
+				mvwprintw(win, Y3, X3 + i, "%c", key - 32);
+			if (key >= 'A' && key <= 'Z')
+				mvwprintw(win, Y3, X3 + i, "%c", key);
+			wattroff(win, COLOR_PAIR(1));
+			wrefresh(win);
+		}
+}
+
 int main(){	
     ITEM **my_items, **items;
 	int c, x, y, key, mkey;	
 	MENU *main_menu, *menu;
     WINDOW *main_menu_win, *my_win, *menu_win;
-    int n_choices, nitems, i;
-	char *choices[] = {"START GAME", "   QUIT"}, *mitems[] = {"NEW GAME", "  QUIT"}, *p;
-	/* Initialize curses */
+    int n_choices, nitems, i, j, xi, yi, word_poz, key_poz;
+	char *choices[] = {"START GAME", "   QUIT"}, *mitems[] = {"NEW GAME", "  QUIT"}, *word, *gitems[] = {"RETRY", "QUIT"},
+	keyboard1[] = {"Q W E R T Y U I O P"}, keyboard2[] = {"A S D F G H J K L"}, keyboard3[] = {"Z X C V B N M"},
+	*word_list[] = {"arici", "atent", "baiat", "ceata", "debut", "peste", "fixat", "hamac", "harta", "jalon", "jucam", "lacat", "magie",
+	"nufar", "oaste", "perus", "rigle", "roman", "sanie", "scris", "sonda", "texte", "tipar", "titan", "zebra", "vapor", "vatra", "regie"};
+	TASTA tasta[5];
 	initscr();
 	keypad(stdscr, TRUE);
     getmaxyx(stdscr, y, x);
@@ -43,46 +107,30 @@ int main(){
     cbreak();
     noecho();
 	init_pair(1, COLOR_CYAN, COLOR_BLACK);
-
-	/* Create items */
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);
+	init_pair(4, COLOR_RED, COLOR_BLACK);
     n_choices = ARRAY_SIZE(choices);
     my_items = (ITEM **)calloc(n_choices, sizeof(ITEM *));
     for(i = 0; i < n_choices; i++){
         my_items[i] = new_item(choices[i], NULL);
     }
-	/* Crate menu */
 	main_menu = new_menu((ITEM **)my_items);
-
-	/* Create the window to be associated with the menu */
     main_menu_win = newwin(39, 80, 6, 53);
     keypad(main_menu_win, TRUE);
-     
-	/* Set main window and sub window */
     set_menu_win(main_menu, main_menu_win);
     set_menu_sub(main_menu, derwin(main_menu_win, 2, 15, 19, 31));
-
     wresize(stdscr, y, x);
-
-	/* Set menu mark to the string */ 
 	set_menu_mark(main_menu, " > ");
-
-    /* Print a border around the main window and print a title */
     box(main_menu_win, 0, 0);
 	print_in_middle(main_menu_win, 1, 4, 70, "Welcome to WORDLE! Press START to begin your adventure", COLOR_PAIR(1));
 	mvwaddch(main_menu_win, 2, 0, ACS_LTEE);
 	mvwhline(main_menu_win, 2, 1, ACS_HLINE, 80);
 	mvwaddch(main_menu_win, 2, 79, ACS_RTEE);
 	refresh();
-        
-	/* Post the menu */
 	post_menu(main_menu);
 	wrefresh(main_menu_win);
-
     curs_set(0);
-
-	p = (char *)malloc(2 * sizeof(char));
-	p[1] = 0;
-
 	while(TRUE)
 	{       
 		c = wgetch(main_menu_win);
@@ -99,8 +147,7 @@ int main(){
 				pos_menu_cursor(main_menu);
 				if (strcmp(item_name(curr), "   QUIT") == 0)
 					goto exit;
-				if (strcmp(item_name(curr), "START GAME") == 0)
-				{
+				if (strcmp(item_name(curr), "START GAME") == 0){
 					newgame:
 					my_win = newwin(39, 80, 6, 53);
 					keypad(my_win, TRUE);
@@ -110,7 +157,19 @@ int main(){
 					mvwaddch(my_win, 2, 0, ACS_LTEE);
 					mvwhline(my_win, 2, 1, ACS_HLINE, 80);
 					mvwaddch(my_win, 2, 79, ACS_RTEE);
+					wattron(my_win, COLOR_PAIR(1));
 					mvwprintw(my_win, 37, 2, "Press : to open the menu");
+					wattroff(my_win, COLOR_PAIR(1));
+					mvwprintw(my_win, Y1, X1, "%s", keyboard1);
+					mvwprintw(my_win, Y2, X2, "%s", keyboard2);
+					mvwprintw(my_win, Y3, X3, "%s", keyboard3);
+					srand(time(NULL));
+					word_poz = random_poz(0, ARRAY_SIZE(word_list) - 1);
+					word = strdup(word_list[word_poz]);
+					mvwprintw(my_win, 4, 30, "%s", word);
+					yi = Y;
+					xi = X;
+					key_poz = 0;
 					refresh();
 					wrefresh(my_win);
 					while(TRUE){
@@ -124,18 +183,14 @@ int main(){
 							menu = new_menu((ITEM**)items);
 							menu_win = newwin(15, 40, 16, 73);
 							keypad(menu_win, TRUE);
-							/* Set main window and sub window */
 							set_menu_win(menu, menu_win);
 							set_menu_sub(menu, derwin(menu_win, 2, 15, 7, 13));
-							/* Set menu mark to the string */ 
 							set_menu_mark(menu, " > ");
-							/* Print a border around the main window and print a title */
 							box(menu_win, 0, 0);
 							print_in_middle(menu_win, 1, 15, 10, "MENU", COLOR_PAIR(1));
 							mvwaddch(menu_win, 2, 0, ACS_LTEE);
 							mvwhline(menu_win, 2, 1, ACS_HLINE, 40);
 							mvwaddch(menu_win, 2, 39, ACS_RTEE);
-							/* Post the menu */
 							post_menu(menu);
 							wrefresh(menu_win);
 							while (TRUE){
@@ -151,27 +206,50 @@ int main(){
 										curr = current_item(menu);
 										pos_menu_cursor(menu);
 										if (strcmp(item_name(curr), "  QUIT") == 0){
-											goto exit;
+											free(word);
 											unpost_menu(menu);
 											free_menu(menu);
 											for(i = 0; i < nitems; i++){
 												free_item(items[i]);}
+											goto exit;
 										}
 										if (strcmp(item_name(curr), "NEW GAME") == 0){
-											goto newgame;
+											free(word);
 											unpost_menu(menu);
 											free_menu(menu);
 											for(i = 0; i < nitems; i++){
 												free_item(items[i]);}
+											goto newgame;
 										}
 									}
 									default: break;
 								}
 							}
 						}
-						else{
-							p[0] = key;
+						if (key == 10 && key_poz != 5){
+							for (i = 0; i < key_poz; i++)
+								mvwprintw(my_win, yi, xi + 2 * i, "%c", ' ');
+							key_poz = 0;
+						}
+						else if (key == 10 && key_poz == 5){
 							
+							for (i = 0; i < key_poz; i++){
+							
+							}
+							key_poz = 0; yi += 2;
+						}
+						else{
+							if(key_poz >= 5)
+								continue;
+							tasta[key_poz].name = key;
+							if(key_poz < 5){
+								color_keyboard(my_win, keyboard1, keyboard2, keyboard3, key);
+								if (key >= 'a' && key <= 'z')
+									mvwprintw(my_win, yi, xi + 2 * key_poz, "%c", key - 32);
+								if (key >= 'A' && key <= 'Z')
+									mvwprintw(my_win, yi, xi + 2 * key_poz, "%c", key);
+							}
+							key_poz++;
 						}
 					}
 					goto exit;
