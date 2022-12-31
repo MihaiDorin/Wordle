969f8e4dd8edb15610f556aff6db471db9d2dfb5
Pacuraru Mihai Dorin <mihaidorin653@gmail.com>
2022-12-31 15:22:26 +0200

    Joc terminat

--- wordle.c
+++ wordle.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 #define X 34
 #define Y 11
@@ -54,38 +55,81 @@ int aparitie (char *word, char c){
 	return 0;
 }
 
-void color_keyboard(WINDOW *win, char *k1, char *k2, char *k3, char key){
+void color_keyboard(WINDOW *win, char *k1, char *k2, char *k3, char key, chtype color){
 	int i;
 	for (i = 0; i < strlen(k1); i++)
 		if (tolower(k1[i]) == tolower(key)){
-			wattron(win, COLOR_PAIR(1));
+			wattron(win, color);
 			if (key >= 'a' && key <= 'z')
 				mvwprintw(win, Y1, X1 + i, "%c", key - 32);
 			if (key >= 'A' && key <= 'Z')
 				mvwprintw(win, Y1, X1 + i, "%c", key);
-			wattroff(win, COLOR_PAIR(1));
+			wattroff(win, color);
 			wrefresh(win);
 		}
 	for (i = 0; i < strlen(k2); i++)
 		if (tolower(k2[i]) == tolower(key)){
-			wattron(win, COLOR_PAIR(1));
+			wattron(win, color);
 			if (key >= 'a' && key <= 'z')
 				mvwprintw(win, Y2, X2 + i, "%c", key - 32);
 			if (key >= 'A' && key <= 'Z')
 				mvwprintw(win, Y2, X2 + i, "%c", key);
-			wattroff(win, COLOR_PAIR(1));
+			wattroff(win, color);
 			wrefresh(win);
 		}
 	for (i = 0; i < strlen(k3); i++)
 		if (tolower(k3[i]) == tolower(key)){
-			wattron(win, COLOR_PAIR(1));
+			wattron(win, color);
 			if (key >= 'a' && key <= 'z')
 				mvwprintw(win, Y3, X3 + i, "%c", key - 32);
 			if (key >= 'A' && key <= 'Z')
 				mvwprintw(win, Y3, X3 + i, "%c", key);
-			wattroff(win, COLOR_PAIR(1));
+			wattroff(win, color);
+			refresh();
 			wrefresh(win);
 		}
+}
+
+void color(WINDOW *win, TASTA *key, char *word, int y, int x, char *k1, char *k2, char *k3){
+	int i;
+	for (i = 0; i < 5; i++){
+		if(tolower(word[i]) == tolower(key[i].name))
+			key[i].poz = 2;
+		if (aparitie(word, key[i].name) == 0)
+			key[i].poz = 0;
+		if (aparitie(word, key[i].name) == 1 && tolower(word[i]) != tolower(key[i].name))
+			key[i].poz = 1;
+	}
+	for (i = 0; i < 5; i++){
+		if(key[i].poz == 0){
+			color_keyboard(win, k1, k2, k3, key[i].name, COLOR_PAIR(4));
+			wattron(win, COLOR_PAIR(4));
+			if(key[i].name >= 'A' && key[i].name <= 'Z')
+				mvwprintw(win, y, x + 2 * i, "%c", key[i].name);
+			if(key[i].name >= 'a' && key[i].name <= 'z')
+				mvwprintw(win, y, x + 2 * i, "%c", key[i].name - 32);
+			wattroff(win, COLOR_PAIR(4));
+		}
+		if(key[i].poz == 1){
+			color_keyboard(win, k1, k2, k3, key[i].name, COLOR_PAIR(2));
+			wattron(win, COLOR_PAIR(2));
+			if(key[i].name >= 'A' && key[i].name <= 'Z')
+				mvwprintw(win, y, x + 2 * i, "%c", key[i].name);
+			if(key[i].name >= 'a' && key[i].name <= 'z')
+				mvwprintw(win, y, x + 2 * i, "%c", key[i].name - 32);
+			wattroff(win, COLOR_PAIR(2));
+		}
+		if(key[i].poz == 2){
+			color_keyboard(win, k1, k2, k3, key[i].name, COLOR_PAIR(3));
+			wattron(win, COLOR_PAIR(3));
+			if(key[i].name >= 'A' && key[i].name <= 'Z')
+				mvwprintw(win, y, x + 2 * i, "%c", key[i].name);
+			if(key[i].name >= 'a' && key[i].name <= 'z')
+				mvwprintw(win, y, x + 2 * i, "%c", key[i].name - 32);
+			wattroff(win, COLOR_PAIR(3));
+		}
+	}
+	wrefresh(win);
 }
 
 int main(){	
@@ -95,10 +139,9 @@ int main(){
     WINDOW *main_menu_win, *my_win, *menu_win;
     int n_choices, nitems, i, j, xi, yi, word_poz, key_poz;
 	char *choices[] = {"START GAME", "   QUIT"}, *mitems[] = {"NEW GAME", "  QUIT"}, *word, *gitems[] = {"RETRY", "QUIT"},
-	keyboard1[] = {"Q W E R T Y U I O P"}, keyboard2[] = {"A S D F G H J K L"}, keyboard3[] = {"Z X C V B N M"},
 	*word_list[] = {"arici", "atent", "baiat", "ceata", "debut", "peste", "fixat", "hamac", "harta", "jalon", "jucam", "lacat", "magie",
 	"nufar", "oaste", "perus", "rigle", "roman", "sanie", "scris", "sonda", "texte", "tipar", "titan", "zebra", "vapor", "vatra", "regie"};
-	TASTA tasta[5];
+	TASTA tasta[5]; char cuvant[5];
 	initscr();
 	keypad(stdscr, TRUE);
     getmaxyx(stdscr, y, x);
@@ -149,6 +192,7 @@ int main(){
 					goto exit;
 				if (strcmp(item_name(curr), "START GAME") == 0){
 					newgame:
+					curs_set(1);
 					my_win = newwin(39, 80, 6, 53);
 					keypad(my_win, TRUE);
 					wresize(stdscr, y, x);
@@ -160,21 +204,32 @@ int main(){
 					wattron(my_win, COLOR_PAIR(1));
 					mvwprintw(my_win, 37, 2, "Press : to open the menu");
 					wattroff(my_win, COLOR_PAIR(1));
+					char keyboard1[] = {"Q W E R T Y U I O P"}, keyboard2[] = {"A S D F G H J K L"}, keyboard3[] = {"Z X C V B N M"};
 					mvwprintw(my_win, Y1, X1, "%s", keyboard1);
 					mvwprintw(my_win, Y2, X2, "%s", keyboard2);
 					mvwprintw(my_win, Y3, X3, "%s", keyboard3);
 					srand(time(NULL));
 					word_poz = random_poz(0, ARRAY_SIZE(word_list) - 1);
 					word = strdup(word_list[word_poz]);
-					mvwprintw(my_win, 4, 30, "%s", word);
 					yi = Y;
 					xi = X;
 					key_poz = 0;
+					wmove(my_win, yi, xi);
 					refresh();
 					wrefresh(my_win);
 					while(TRUE){
 						key = wgetch(my_win);
+						if (key == KEY_BACKSPACE){
+							if (key_poz > 0)
+								key_poz--;
+							mvwprintw(my_win, yi, xi + 2 * key_poz, "%c", ' ');
+							if (key_poz > 0)
+								wmove(my_win, yi, xi + 2 * key_poz);
+							if (key_poz == 0)
+								wmove(my_win, yi, xi);
+						}
 						if (key == ':'){
+							curs_set(0);
 							nitems = ARRAY_SIZE(mitems);
 							items = (ITEM **)calloc(nitems, sizeof(ITEM *));
 							for(i = 0; i < nitems; i++){
@@ -230,26 +285,139 @@ int main(){
 							for (i = 0; i < key_poz; i++)
 								mvwprintw(my_win, yi, xi + 2 * i, "%c", ' ');
 							key_poz = 0;
+							wmove(my_win, yi, xi);
 						}
 						else if (key == 10 && key_poz == 5){
-							
-							for (i = 0; i < key_poz; i++){
-							
+							for (i = 0; i < 5; i++)
+								cuvant[i] = tasta[i].name;
+							cuvant[key_poz] = 0;
+							char keyboard1[] = {"Q W E R T Y U I O P"}, keyboard2[] = {"A S D F G H J K L"}, keyboard3[] = {"Z X C V B N M"};
+							color(my_win, tasta, word, yi, xi, keyboard1, keyboard2, keyboard3);
+							if (strcmp(cuvant, word) == 0){
+								curs_set(0);
+								nitems = ARRAY_SIZE(mitems);
+								items = (ITEM **)calloc(nitems, sizeof(ITEM *));
+								for(i = 0; i < nitems; i++){
+									items[i] = new_item(mitems[i], NULL);
+								}
+								menu = new_menu((ITEM**)items);
+								menu_win = newwin(15, 40, 16, 73);
+								keypad(menu_win, TRUE);
+								set_menu_win(menu, menu_win);
+								set_menu_sub(menu, derwin(menu_win, 2, 15, 7, 13));
+								set_menu_mark(menu, " > ");
+								box(menu_win, 0, 0);
+								print_in_middle(menu_win, 1, 15, 10, "YOU WON", COLOR_PAIR(3));
+								mvwaddch(menu_win, 2, 0, ACS_LTEE);
+								mvwhline(menu_win, 2, 1, ACS_HLINE, 40);
+								mvwaddch(menu_win, 2, 39, ACS_RTEE);
+								post_menu(menu);
+								refresh();
+								wrefresh(menu_win);
+								while (TRUE){
+									mkey = wgetch(menu_win);
+									switch (mkey){
+										case KEY_DOWN:
+											menu_driver(menu, REQ_DOWN_ITEM);
+											break;
+										case KEY_UP:
+											menu_driver(menu, REQ_UP_ITEM);
+											break;
+										case 10: {
+											curr = current_item(menu);
+											pos_menu_cursor(menu);
+											if (strcmp(item_name(curr), "  QUIT") == 0){
+												free(word);
+												unpost_menu(menu);
+												free_menu(menu);
+												for(i = 0; i < nitems; i++){
+													free_item(items[i]);}
+												goto exit;
+											}
+											if (strcmp(item_name(curr), "NEW GAME") == 0){
+												free(word);
+												unpost_menu(menu);
+												free_menu(menu);
+												for(i = 0; i < nitems; i++){
+													free_item(items[i]);}
+												goto newgame;
+											}
+										}
+										default: break;
+									}
+								}
 							}
-							key_poz = 0; yi += 2;
+							if (yi - Y == 10 && strcmp(cuvant, word) != 0){
+								curs_set(0);
+								nitems = ARRAY_SIZE(gitems);
+								items = (ITEM **)calloc(nitems, sizeof(ITEM *));
+								for(i = 0; i < nitems; i++){
+									items[i] = new_item(gitems[i], NULL);
+								}
+								menu = new_menu((ITEM**)items);
+								menu_win = newwin(15, 40, 16, 73);
+								keypad(menu_win, TRUE);
+								set_menu_win(menu, menu_win);
+								set_menu_sub(menu, derwin(menu_win, 2, 15, 7, 13));
+								set_menu_mark(menu, " > ");
+								box(menu_win, 0, 0);
+								print_in_middle(menu_win, 1, 15, 10, "GAME OVER", COLOR_PAIR(4));
+								mvwaddch(menu_win, 2, 0, ACS_LTEE);
+								mvwhline(menu_win, 2, 1, ACS_HLINE, 40);
+								mvwaddch(menu_win, 2, 39, ACS_RTEE);
+								post_menu(menu);
+								refresh();
+								wrefresh(menu_win);
+								while (TRUE){
+									mkey = wgetch(menu_win);
+									switch (mkey){
+										case KEY_DOWN:
+											menu_driver(menu, REQ_DOWN_ITEM);
+											break;
+										case KEY_UP:
+											menu_driver(menu, REQ_UP_ITEM);
+											break;
+										case 10: {
+											curr = current_item(menu);
+											pos_menu_cursor(menu);
+											if (strcmp(item_name(curr), "QUIT") == 0){
+												free(word);
+												unpost_menu(menu);
+												free_menu(menu);
+												for(i = 0; i < nitems; i++){
+													free_item(items[i]);}
+												goto exit;
+											}
+											if (strcmp(item_name(curr), "RETRY") == 0){
+												free(word);
+												unpost_menu(menu);
+												free_menu(menu);
+												for(i = 0; i < nitems; i++){
+													free_item(items[i]);}
+												goto newgame;
+											}
+										}
+										default: break;
+									}
+								}
+							}
+							key_poz = 0; yi += 2; wmove(my_win, yi, xi);
 						}
+						else if ((key < 'a' || key > 'z') && (key < 'A' || key > 'Z'))
+							continue;
 						else{
 							if(key_poz >= 5)
 								continue;
-							tasta[key_poz].name = key;
 							if(key_poz < 5){
-								color_keyboard(my_win, keyboard1, keyboard2, keyboard3, key);
+								tasta[key_poz].name = tolower(key);
 								if (key >= 'a' && key <= 'z')
 									mvwprintw(my_win, yi, xi + 2 * key_poz, "%c", key - 32);
 								if (key >= 'A' && key <= 'Z')
 									mvwprintw(my_win, yi, xi + 2 * key_poz, "%c", key);
 							}
 							key_poz++;
+							if (key_poz < 5)
+								wmove(my_win, yi, xi + 2 * key_poz);
 						}
 					}
 					goto exit;
