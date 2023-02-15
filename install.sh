0a201ee6883805d3b812bdb9f094f9c79bbd33ab
Pacuraru Mihai Dorin <mihaidorin653@gmail.com>
2023-02-15 14:59:33 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-# install xterm required for playing the game
+MSG="Usage: ./install.sh [option]\n\n\tOptions: -u (uninstall) | -h (help)\n\n\tType ./install.sh to install xterm and compile the game.\n\n\t\
+Use 'make run' to play the game.\n\n\tType './install.sh -u' to uninstall xterm and clean the workspace.\n"
+
 if test $# == 0; then
     if test -f /usr/bin/xterm 2> /dev/null; then
         echo "XTerm is already installed"
     else
         sudo apt update
-        sudo apt install xterm
+        sudo apt install -y xterm
     fi
     # compile the game
-        make
+    make
 fi
 
-if test $1 == uninstall; then
-    make clean
+if test $# == 1; then
+    if test $1 = -u; then
+        # clean the workspace
+        make clean
+    elif test $1 = -h; then
+        echo -e $MSG
+    else
+        echo -e $MSG
+    fi
 fi
